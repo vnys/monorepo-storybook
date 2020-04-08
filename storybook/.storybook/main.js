3b01af5d6bc2731c56d7db1d43d5048d06e59acf
Victor Nystad <vnys@equinor.com>
2020-04-08 14:20:43 +0200

    add babel-plugin-react-docgen

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'],
+          plugins: ['react-docgen'],
         },
       },
     })
